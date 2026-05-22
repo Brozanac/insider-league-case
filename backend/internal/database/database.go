9a7e93faff8bd81b5fb86109a6137c50c05ff42c
Ulaş Aktaş <93611335+Brozanac@users.noreply.github.com>
2026-05-22 21:17:11 +0300

    Update database.go

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -2,12 +2,17 @@ package database
 
 import (
 	"insider-league-case/internal/models"
+	"os"
 
 	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 )
 
 func Connect() (*gorm.DB, error) {
+	if err := os.MkdirAll("data", os.ModePerm); err != nil {
+		return nil, err
+	}
+
 	db, err := gorm.Open(sqlite.Open("data/league.db"), &gorm.Config{})
 	if err != nil {
 		return nil, err
